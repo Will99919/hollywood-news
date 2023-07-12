cf9084a308e77ac0db18b905544b13d29ceac117
Will99919 <danjouloic@live.fr>
2023-07-12 12:35:42 +0200

    ajout des fichiers

---
+++ moovies.php
@@ -0,0 +1,214 @@
+<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/retourverslefuture.jpg" alt="Retour vers le future" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Retour vers le future</h2>
+                        <p class="card-text">Marty McFly, un lycéen de 17 ans, est accidentellement envoyé trente ans plus tôt avec une voiture DeLorean qui voyage dans le temps, inventé par son ami le scientifique non-conformiste Doc Brown.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 30 Octobre 1985 (France)</small></p>
+                        <a href="./retourverslefuture.php">En savoir plus</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/halloween.webp" alt="Halloween" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Halloween</h2>
+                        <p class="card-text">Quinze ans après avoir assassiné sa soeur le soir d'Halloween en 1963, Michael Myers s'échappe d'un hôpital psychiatrique et retourne dans la petite ville de Haddonfield pour tuer à nouveau.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 14 mars 1979 (France)</small></p>
+                        <a href="./halloween.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/LES-AFFRANCHIS.jpg" alt="Les affranchis" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                       <h2 class="card-title">Les affranchis</h2>
+                        <p class="card-text">L'histoire d'Henry Hill et de sa vie dans la mafia, couvrant ses relations avec son épouse Karen Hill et ses partenaires de la mafia Jimmy Conway et Tommy DeVito dans le syndicat du crime italo-américain.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 12 Octobre 1990 (France)</small></p>
+                        <a href="./affranchis.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/reservoirdogs.jpg" alt="Reservoir Dogs" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Reservoir Dogs</h2>
+                        <p class="card-text">Un chef de la mafia réunit une équipe de six criminels expérimentés dans le but de réaliser un grand braquage de bijouterie.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 2 Septembre 1992 (France)</small></p>
+                        <a href="./reservoir.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>  
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/highlander.jpg" alt="Highlander" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Highlander</h2>
+                        <p class="card-text">"Un jeune guerrier écossais immortel doit affronter son ultime adversaire, un barbare meurtrier et brutal qui aspire au légendaire ""Prix""."</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 12 Avril 1992 (France)</small></p>
+                        <a href="./highlander.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/spiderman3.jpg" alt="Spiderman 3" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Spiderman 3</h2>
+                        <p class="card-text">Une étrange et sombre entité venue d'un autre monde se lie à Peter Parker et provoque un chaos intérieur alors qu'il affronte de nouveaux malfaiteurs ainsi que de nouvelles tentations et vengeances.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 1 mai 2007 (France)</small></p>
+                        <a href="./spiderman.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/scanners.jpg" alt="Scanner" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">scanners</h2>
+                        <p class="card-text">Un scientifique recrute un homme doté de pouvoirs psychiques extraordinaires pour qu'ils puisse détecter d'autres personnes comme lui.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 8 avril 1981 (France)</small></p>
+                        <a href="./scanners.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/johnwick.webp" alt="John wick" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">John Wick</h2>
+                        <p class="card-text">Un ancien tueur à gages sort de sa retraite pour retrouver les gangsters qui ont tué son chien et lui a tout pris.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 29 Octobre 2014 (France)</small></p>
+                        <a href="./johnwick.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/LOTR.jpg" alt="Le seigneur des anneaux" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Le seigneur des anneaux</h2>
+                        <p class="card-text">Un Hobbit du Comté et ses huit compagnons se mettent en route pour détruire le puissant anneau unique et sauver la Terre du Milieu du terrible Sauron.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 19 Decembre 2001 (France)</small></p>
+                        <a href="./lotr.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/kaamelott.webp" alt="Kaamelott" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Kaamelott</h2>
+                        <p class="card-text">Le tyrannique Lancelot-du-Lac et ses mercenaires saxons font régner la terreur sur le royaume de Logres. Arthur parviendra-t-il à fédérer les clans rebelles, renverser son rival, reprendre Kaamelott et restaurer la paix en Bretagne ?</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 20 juillet 2021 (France)</small></p>
+                        <a href="./kaamelott.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/jaws.jpg" alt="Les dents de la mer" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Les dents de la mer</h2>
+                        <p class="card-text">Un shérif local, un biologiste marin et un vieux marin font équipe pour chasser un grand requin blanc qui fait des ravages dans une station balnéaire.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 1 Janvier 1976 (France)</small></p>
+                        <a href="./jaws.php">En savoir plus</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="./img/wisplash.jpg" alt="Wisplash" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Wisplash</h2>
+                        <p class="card-text">Un jeune batteur prometteur s'inscrit dans un conservatoire de musique renommé où ses rêves de grandeur sont encadrés par un professeur qui ne recule devant rien pour réaliser le potentiel de ses étudiants.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 24 décembre 2014 (France)</small></p>
+                        <a href="./wisplash.php">En savoir plus</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
